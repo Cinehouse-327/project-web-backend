6e7671264fdba137c996db7f3f25a049aa500a00
Sazzad Hossain <sazzadhossainadib@gmail.com>
2024-11-26 16:05:45 +0600

    seat booking done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors";
 import { errorMiddlewares } from "./middlewares/error.js";
 import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/sazzad/userRoute.js"
 
 
 /**
@@ -72,7 +73,7 @@ app.use("/auth", authRoutes);
  * @description Route handler for user-related requests.
  * All routes under "/user" will be handled by userRoutes.
  */
-// app.use("/user", userRoutes);
+app.use("/user", userRoutes);
 
 
 /**
